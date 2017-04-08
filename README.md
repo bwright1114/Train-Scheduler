74d13cf58d072d9b0654649cdd5fd671a512f600
Brittni Wright <bwright1114@icloud.com>
2017-04-08 12:42:08 -0400

    final updates and push

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Train-Sceduler
+# Train-Scheduler
 create a train schedule application that incorporates Firebase to host arrival and departure data. app will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,35 @@
+tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+function GetClock(){
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+if(nyear<1000) nyear+=1900;
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+
+if(nhour==0){ap=" AM";nhour=12;}
+else if(nhour<12){ap=" AM";}
+else if(nhour==12){ap=" PM";}
+else if(nhour>12){ap=" PM";nhour-=12;}
+
+if(nmin<=9) nmin="0"+nmin;
+if(nsec<=9) nsec="0"+nsec;
+
+document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
+}
+
+window.onload=function(){
+GetClock();
+setInterval(GetClock,1000);
+}
+
+
+
+
 $(document).ready(function(){
   
 
-  // Init Firebase
+// Init Firebase
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD6GQzaD7HphW_iyg5IXnlsLecD1_aVDQo",
@@ -15,6 +43,7 @@ $(document).ready(function(){
 
   
  var database = firebase.database();
+
 
   // on click for form submission
     $("#submit").click(function(){
@@ -49,7 +78,7 @@ $(document).ready(function(){
       console.log("Frequency: " + frequency);
       console.log(moment().format("hh:mm"));
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
+  
     var firstTimeConverted = moment(time,"hh:mm").subtract(1, "years");
     console.log(firstTimeConverted);
     // Current Time
@@ -67,8 +96,9 @@ $(document).ready(function(){
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));  
-  
-    var newElement = $("<tr/>").attr("data-name", name);
+
+ 
+  var newElement = $("<tr/>").attr("data-name", name);
     newElement.append($("<td/> ").text(name));
     newElement.append($("<td/> ").text(destination));
     newElement.append($("<td/> ").text(frequency));
@@ -79,7 +109,3 @@ $(document).ready(function(){
 
  }); 
 })
-
-
-// child added
-
